4628df73a317f23974b00dea9764c7f15eab1657
johnromanoff <johnromanoff@yandex.ru>
2019-02-12 16:21:45 +0300

    init

---
+++ src/points/core.clj
@@ -0,0 +1,6 @@
+(ns points.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/points/core_test.clj
@@ -0,0 +1,7 @@
+(ns points.core-test
+  (:require [clojure.test :refer :all]
+            [points.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
