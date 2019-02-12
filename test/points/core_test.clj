c6863c4d04e03f262ba6d6a3d26c698a6cff5713
johnromanoff <johnromanoff@yandex.ru>
2019-02-12 23:18:28 +0300

    add tests for points function

--- test/points/core_test.clj
+++ test/points/core_test.clj
@@ -4,4 +4,12 @@
 
 (deftest a-test
   (testing "make-point"
-    (is (= 0 1))))
+    (is (= "(1, 2)" (to-string (make-point 1 2))))
+    (is (= "(0, 0)" (to-string (make-point 0 0))))
+    (is (= "(-1, -2)" (to-string (make-point -1 -2)))))
+  (testing "get-x" 
+    (is (= 1 (get-x (make-point 1 2))))
+    (is (= -1 (get-x (make-point -1 2)))))
+  (testing "get-y"
+    (is (= 2 (get-y (make-point 1 2))))
+    (is (= 3 (get-y (make-point -1 3))))))
