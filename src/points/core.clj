72d6d98f70351c6ae44adbea2afc5a87661c5065
johnromanoff <johnromanoff@yandex.ru>
2019-02-13 04:17:26 +0300

    added functions

--- src/points/core.clj
+++ src/points/core.clj
@@ -9,3 +9,22 @@
 (defn to-string 
   [point] 
   (str "(" (get-x point) ", " (get-y point) ")"))
+
+(defn get-quadrant
+  [point] 
+  (let [x (get-x point) 
+        y (get-y point)]
+    (cond 
+      (or (= x 0) (= y 0)) 0
+      (and (> x 0) (> y 0)) 1
+      (and (< x 0) (> y 0)) 2 
+      (and (< x 0) (< y 0)) 3 
+      :else 4)))
+
+(defn get-sym-point [point] 
+  (make-point (- (get-x point)) (- (get-y point))))
+
+(defn calc-distance [p1 p2]
+  (let [x (square (- (get-x p2) (get-x p1))) 
+        y (square (- (get-y p2) (get-y p1)))]
+    (sqrt (+ x y))))
--- test/points/core_test.clj
+++ test/points/core_test.clj
@@ -12,4 +12,16 @@
     (is (= -1 (get-x (make-point -1 2)))))
   (testing "get-y"
     (is (= 2 (get-y (make-point 1 2))))
-    (is (= 3 (get-y (make-point -1 3))))))
+    (is (= 3 (get-y (make-point -1 3)))))
+  (testing "get-quadrant" 
+    (is (= 0 (get-quadrant (make-point 0 2))))
+    (is (= 0 (get-quadrant (make-point 1 0))))
+    (is (= 1 (get-quadrant (make-point 1 2))))
+    (is (= 2 (get-quadrant (make-point -1 2))))
+    (is (= 3 (get-quadrant (make-point -1 -2))))
+    (is (= 4 (get-quadrant (make-point 1 -2)))))
+  (testing "get-sym-point" 
+    (is (= (to-string (make-point -1 -2)) (to-string (get-sym-point (make-point 1 2)))))
+    (is (= (to-string (make-point 1 -2)) (to-string (get-sym-point (make-point -1 2)))))
+    (is (= (to-string (make-point -1 2)) (to-string (get-sym-point (make-point 1 -2)))))
+    (is (= (to-string (make-point 1 2)) (to-string (get-sym-point (make-point -1 -2)))))))
